ed1f9a1e6ec9aa4cb1705afcb168d06d74921c90
chenshic.hub <chenshiq@hu-berlin.de>
2016-07-29 14:17:41 +0200

    update codes

--- econ_vola/econ_vola.R
+++ econ_vola/econ_vola.R
@@ -1,55 +1,56 @@
-rm(list=ls(all=TRUE))
+rm(list = ls(all = TRUE))
 graphics.off()
 
-#require(forecast)
+# require(forecast)
 require(tseries)
 require(FinTS)
 
 # please change your working directory
 setwd("~/EconCrix/econ_vola")
 
-load(file="crix.RData")
+load(file = "crix.RData")
 Pr = as.numeric(crix)
-Da= factor(date1)
+Da = factor(date1)
 crx = data.frame(Da, Pr)
 
-#plot of crix return
+# plot of crix return
 ret = diff(log(crx$Pr))
 Dare = factor(date1[-1])
 retts = data.frame(Dare, ret)
 
-#comparison of different crix returns
-par(mfrow=c(2,2))
-plot(crx$Da, crx$Pr, type="o")
+# comparison of different crix returns
+par(mfrow = c(2, 2))
+plot(crx$Da, crx$Pr, type = "o")
 lines(crx$Pr)
-plot(crx$Da, log(crx$Pr), type="o")
+plot(crx$Da, log(crx$Pr), type = "o")
 lines(log(crx$Pr))
-plot(retts$Dare, diff(crx$Pr), type="o")
+plot(retts$Dare, diff(crx$Pr), type = "o")
 lines(diff(crx$Pr))
-plot(retts$Dare, retts$ret, type="o")
+plot(retts$Dare, retts$ret, type = "o")
 lines(retts$ret)
 
-#ARIMAfit <- auto.arima(ret, approximation=FALSE,trace=FALSE)
-#summary(ARIMAfit)
+# ARIMAfit <- auto.arima(ret, approximation=FALSE,trace=FALSE)
+# summary(ARIMAfit)
 
-######arima202 predict
-fit202 = arima(ret, order=c(2,0,2))
-#tsdiag(fit202)
+# arima202 predict
+fit202 = arima(ret, order = c(2, 0, 2))
 
-#vola cluster
-par(mfrow=c(1,1))
+# vola cluster
+par(mfrow = c(1, 1))
 res = fit202$residuals
 res2 = fit202$residuals^2
 tsres202 = data.frame(Dare, res2)
-plot(tsres202$Dare, tsres202$res2, type="o", ylab=NA)
+plot(tsres202$Dare, tsres202$res2, type = "o", ylab = NA)
 lines(tsres202$res2)
 
-par(mfrow=c(1,2))
-#plot(res2, ylab="Squared residuals", main=NA)
-acfres2 = acf(res2, ylab=NA, lag.max = 20, main="ACF of Squared Residuals", lwd = 2)
-pacfres2 = pacf(res2, lag.max = 20, main="PACF of Squared Residuals", lwd = 2, ylab=NA)
+par(mfrow = c(1, 2))
+# plot(res2, ylab='Squared residuals', main=NA)
+acfres2 = acf(res2, main = NA, lag.max = 20, ylab = "Sample Autocorrelation", 
+              lwd = 2)
+pacfres2 = pacf(res2, lag.max = 20, ylab = "Sample Partial Autocorrelation", 
+                lwd = 2, main = NA)
 
-#arch effect
+# arch effect
 res = fit202$residuals
-ArchTest(res)#library FinTS
-Box.test(res2, type="Ljung-Box")
+ArchTest(res)  #library FinTS
+Box.test(res2, type = "Ljung-Box")
