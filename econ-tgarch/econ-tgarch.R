3d09a244fa4223617f47f0fc854bfec2abcb0ea0
SHIccc <chenshiq@hu-berlin.de>
2019-03-14 22:32:01 +0000

    square qq plot
    add a square qq plot

--- econ-tgarch/econ-tgarch.R
+++ econ-tgarch/econ-tgarch.R
@@ -2,7 +2,7 @@ rm(list = ls(all = TRUE))
 graphics.off()
 
 # install and load packages
-libraries = c("FinTS", "tseries", "forecast", "fGarch")
+libraries = c( "tseries", "forecast", "fGarch", "bsts")
 lapply(libraries, function(x) if (!(x %in% installed.packages())) {
   install.packages(x)
 })
@@ -37,3 +37,34 @@ stu.pacfres2 = pacf(stu.fg11res2, lag.max = 20, main = "PACF of Squared Residual
 # ARIMA-t-GARCH qq plot
 par(mfrow = c(1, 1))
 plot(fg11stu, which = 13)
+
+### fGarch:::.plot.garch.13
+library(bsts)
+aa=function (x, ...) 
+{
+  sres = residuals(x, standardize = TRUE)
+  cond.dist = x@fit$params$cond.dist
+  cond.dist = paste("q", cond.dist, sep = "")
+  nc = nchar(x@fit$params$cond.dist)
+  parNames <- names(x@fit$par)
+  skew <- if ("skew" %in% parNames) 
+    x@fit$par["skew"]
+  else x@fit$params$skew
+  shape <- if ("shape" %in% parNames) 
+    x@fit$par["shape"]
+  else x@fit$params$shape
+  if (cond.dist == "qnorm" || cond.dist == "qQMLE") 
+    .qqDist(sres, dist = "qnorm")
+  if (cond.dist == "qstd" | cond.dist == "qged") 
+    .qqDist(sres, dist = cond.dist, nu = shape,ylim=c(-6.7,6.7))
+  if (cond.dist == "qsnorm") 
+    .qqDist(sres, dist = cond.dist, xi = skew)
+  if (cond.dist == "qsstd" | cond.dist == "qsged") 
+    .qqDist(sres, dist = cond.dist, xi = skew, nu = shape)
+  if (cond.dist == "qsnig") 
+    .qqDist(sres, dist = ".qsnigC", rho = skew, zeta = shape)
+}
+
+aa(x = fg11stu)
+
+  
